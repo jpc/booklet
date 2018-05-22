c01091def5e772258026974f1ddbb7dc9c2a9e23
Jakub Piotr Cłapa <jpc@loee.pl>
2018-05-22 22:50:25 +0200

    Add support for passing in full bounding boxes instead of crop offsets

--- booklet.py
+++ booklet.py
@@ -5,8 +5,11 @@ import sys, os
 def mm(mm):
   return mm / 25.4 * 72
 
-def fitPage (p, tw, th, xo, yo, flip, cx = 0, cy = 0):
-  w, h = p.cropBox.getWidth() - cx, p.cropBox.getHeight() - cy
+def fitPage (p, tw, th, xo, yo, flip, crop):
+  iw, ih = float(p.cropBox.getWidth()), float(p.cropBox.getHeight())
+  cbx, cby = p.cropBox.lowerLeft
+  cbx, cby = cbx + crop.x * iw, cby + (1 - crop.y - crop.h) * ih
+  w, h = float(iw * crop.w), float(ih * crop.h)
   sx, sy = tw / float(w), th / float(h)
   s = min (sx, sy)
   nw, nh = w * s, h * s
@@ -16,8 +19,9 @@ def fitPage (p, tw, th, xo, yo, flip, cx = 0, cy = 0):
     s = -s
     xo += tw
     yo += th
+  xo -= float(cbx) * s
+  yo -= float(cby) * s
   xo += (tw - nw) / 2 * om; yo += (th - nh) / 2 * om
-  xo -= cx/2. * s; yo -= cy/2. * s
   p.addTransformation([s,  0,
                        0,  s,
                        xo, yo])
@@ -60,13 +64,24 @@ def bookletPages (pdf):
 def mergePage (d, s):
   d.mergePage (s)
 
-def getIntOpt (i, d = 0):
+def getFloatOpt (i, d = 0):
   if i >= len(sys.argv):
     return d
-  return int (sys.argv[i])
+  return float (sys.argv[i])
 
-cx = mm (getIntOpt (2))
-cy = mm (getIntOpt (3))
+class crop:
+  pass
+
+evencrop = crop()
+evencrop.x = getFloatOpt (2)
+evencrop.y = getFloatOpt (3)
+evencrop.w = getFloatOpt (4)
+evencrop.h = getFloatOpt (5)
+oddcrop = crop()
+oddcrop.x = getFloatOpt (6, evencrop.x)
+oddcrop.y = getFloatOpt (7, evencrop.y)
+oddcrop.w = getFloatOpt (8, evencrop.w)
+oddcrop.h = getFloatOpt (9, evencrop.h)
 fin = sys.argv[1]
 
 out = PdfFileWriter()
@@ -76,9 +91,9 @@ w, h = mm(297), mm(210)
 for p1, p2, flip in bookletPages(inp):
   p = out.addBlankPage (w, h)
   if (p1):
-    mergePage (p, fitPage (p1, w/2, h, 0, 0, flip, cx, cy))
+    mergePage (p, fitPage (p1, w/2, h, 0, 0, flip, oddcrop))
   if (p2):
-    mergePage (p, fitPage (p2, w/2, h, w/2, 0, flip, cx, cy))
+    mergePage (p, fitPage (p2, w/2, h, w/2, 0, flip, evencrop))
 
 outf = file (os.path.splitext (fin)[0] + ".booklet.pdf", "wb")
 out.write (outf)
